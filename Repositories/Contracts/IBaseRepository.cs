7022781b13addb040f1f23fdca2f1343d61bea41
bnula <bnulagnula@gmail.com>
2021-08-23 21:52:10 +0200

    add repository for Employee model

---
+++ Repositories/Contracts/IBaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvImport.Repositories
+{
+    public interface IBaseRepository<T> where T: class
+    {
+        Task<IEnumerable<T>> FindAllAsync();
+        Task<T> FindByIdAsync(int id);
+        Task<bool> CreateAsync(T entity);
+        Task<bool> DeleteAsync(T entity);
+        Task<bool> UpdateAsync(T entity);
+        Task<bool> SaveAsync();
+    }
+}
---
+++ Repositories/EmployeeRepository.cs
@@ -0,0 +1,58 @@
+﻿using CsvImport.Data;
+using CsvImport.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvImport.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly DataContext _db;
+
+        public EmployeeRepository(DataContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<bool> CreateAsync(Employee entity)
+        {
+            await _db.Employees.AddAsync(entity);
+            return await SaveAsync();
+        }
+
+        public async Task<bool> DeleteAsync(Employee entity)
+        {
+            _db.Employees.Remove(entity);
+            return await SaveAsync();
+        }
+
+        public async Task<IEnumerable<Employee>> FindAllAsync()
+        {
+            var items = await _db.Employees.ToListAsync();
+            return items;
+        }
+
+        public async Task<Employee> FindByIdAsync(int id)
+        {
+            var item = await _db.Employees
+                .Where(i => i.Id == id)
+                .SingleOrDefaultAsync();
+            return item;
+        }
+
+        public async Task<bool> SaveAsync()
+        {
+            return await _db.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> UpdateAsync(Employee entity)
+        {
+            _db.Employees.Update(entity);
+            return await SaveAsync();
+        }
+    }
+}
