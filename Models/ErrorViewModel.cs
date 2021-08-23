c8d68af3329cc233943f88a36fe9640e8b052bfe
bnula <bnulagnula@gmail.com>
2021-08-23 20:49:51 +0200

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CsvImport.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
